5e018af6402f7105c0fb8f573d0fdfb003355f1d
Julia Fong <Juliamzfong@gmail.com>
2020-10-15 19:30:16 -0500

    Add express notes

--- src/app.js
+++ src/app.js
@@ -4,9 +4,30 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send()
+    res.send('Hello express framework!')
 });
+
+app.get('/help', (req, res) => {
+    res.send('Help page')
+    console.log('help page')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About us')
+    console.log(hiiii)
+})
+
+app.get('/weather', (req, res) => {
+    res.send('Weather update')
+    console.log('theres a 90% chance its already raining')
+})
+
 
 //app.com
 //app.com/help
 //app.com/about
+
+app.listen(3000, () => {
+    console.log("Servr is workingggg")
+})
+//port 3000 
