47aa865f4bbfcf16d25cf188b5e91db7a684a159
Julia Fong <Juliamzfong@gmail.com>
2020-10-16 21:52:06 -0500

    Add read me

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello express framework!')
+    res.send('<h1>Weather</h1>')
 });
 
 app.get('/help', (req, res) => {
