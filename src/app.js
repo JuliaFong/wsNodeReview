0017f73d04340331b0bb09f136f9a2e6885cdb87
Julia Fong <Juliamzfong@gmail.com>
2020-10-17 21:28:35 -0500

    Add files to public folder

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
+const path = require('path')
 const express = require('express')
 
 
+
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath)) 
 
 app.get('', (req, res) => {
     res.send('<h1>Weather App</h1>')
