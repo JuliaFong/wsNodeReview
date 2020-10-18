fd336b685914ebd79d326b52ac3c916813f63093
Julia Fong <Juliamzfong@gmail.com>
2020-10-18 18:19:30 -0500

    Refactor code and add hbs files

--- src/app.js
+++ src/app.js
@@ -10,22 +10,24 @@ app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath)) 
 
 app.get('', (req, res) => {
-    res.send('<h1>Weather App</h1>')
-});
-
-app.get('/help', (req, res) => {
-    res.send({
-        name: 'Ben',
-        age: 99
-    }, {
-        name: 'Sandra'
+    res.render('index', {
+        title: 'Weather App!',
+        name: 'Audrey Hepburn'
     })
-    
 })
 
 app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>')
- 
+    res.render('about', {
+        title: 'About Me', 
+        name: 'Audrey Hepburn'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('about', {
+        title: 'Help page',
+        paragraph: 'Hello here is the help page. Below are some FAQ. If your question has not been answered, send an email please.'
+    })
 })
 
 app.get('/weather', (req, res) => {
@@ -36,12 +38,34 @@ app.get('/weather', (req, res) => {
     
 })
 
+app.listen(3000, () => {
+    console.log("Servr is workingggg")
+})
+
+
+
+// app.get('', (req, res) => {
+//     res.send('<h1>Weather App</h1>')
+// });
+
+// app.get('/help', (req, res) => {
+//     res.send({
+//         name: 'Ben',
+//         age: 99
+//     }, {
+//         name: 'Sandra'
+//     })
+    
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send('<h1>About</h1>')
+ 
+// })
+
 
 //app.com
 //app.com/help
 //app.com/about
 
-app.listen(3000, () => {
-    console.log("Servr is workingggg")
-})
 //port 3000 
