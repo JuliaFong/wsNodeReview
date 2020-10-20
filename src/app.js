5449bf6abee63149177a3042f413b7db75ab1d0d
Julia Fong <Juliamzfong@gmail.com>
2020-10-19 19:52:20 -0500

    Add view path to app.js

--- src/app.js
+++ src/app.js
@@ -4,10 +4,21 @@ const express = require('express')
 
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = pth.join(__dirname, '../templates')
+
+
+//Setup handlebars engine and views
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+//Setup static directory to serve
 app.use(express.static(publicDirectoryPath)) 
+
+
 
 app.get('', (req, res) => {
     res.render('index', {
